707b945d90fc5b9b82b4c55c13383ddcbd9683af
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-25 02:31:40 +0000

    message

--- Best50Films2018/lib/Best50Films2018/cli.rb
+++ Best50Films2018/lib/Best50Films2018/cli.rb
@@ -1,5 +1,8 @@
 class Best50Films2018::CLI
 def run
-  puts "Hello welcome to this project"
+  puts "Hello welcome to this Movie info application"
+#html = open(https://www.thrillist.com/entertainment/nation/best-movies-of-2018)
+doc = Nokogiri::HTML(html)
+ puts doc.css(body-text)
 end
 end
--- Best50Films2018/lib/Best50_Films_2018/cli.rb
+++
@@ -1,8 +0,0 @@
-"class Best50Films2018::CLI
-def run
-  puts "Hello welcome to this project"
-html = open(https://www.thrillist.com/entertainment/nation/best-movies-of-2018)
-doc = Nokogiri::HTML(html)
- puts doc.css(body-text)
-end
-end
