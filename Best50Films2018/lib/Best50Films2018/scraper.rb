ec68e1f78d02d97f4b467e74e71ec0cbc19ab0bf
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-26 01:32:17 +0000

    move cli code to scraper classe

--- Best50Films2018/lib/Best50Films2018/scraper.rb
+++ Best50Films2018/lib/Best50Films2018/scraper.rb
@@ -1,3 +1,9 @@
 class Best50Films2018::Scraper
+  SITE ='https://www.thrillist.com/entertainment/nation/best-movies-of-2018'
+  def scrape_movie_info
+    html = open(SITE)
+  doc = Nokogiri::HTML(html)
+  binding.pry
 
+end
 end
