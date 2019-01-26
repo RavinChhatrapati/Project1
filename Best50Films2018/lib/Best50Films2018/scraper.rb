295cdcfc2d64cb03b7e9b9a667596214e46b8d35
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-26 02:35:40 +0000

    location of movie info in html found

--- Best50Films2018/lib/Best50Films2018/scraper.rb
+++ Best50Films2018/lib/Best50Films2018/scraper.rb
@@ -3,7 +3,8 @@ class Best50Films2018::Scraper
   def scrape_movie_info
     html = open(SITE)
   doc = Nokogiri::HTML(html)
-  binding.pry
+   puts doc.css("section.body-text.u-color--dark-gray.u-color--entertainment-links.font--body.has-mobile-padding.is-standard")[1..-2]
+binding.pry
 
 end
 end
