bbddde3a40859d7c1948b0af9753cde19b35bad7
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-27 18:14:47 +0000

    issue with line 8 for scraper class

--- Best50Films2018/lib/Best50Films2018/movie.rb
+++ Best50Films2018/lib/Best50Films2018/movie.rb
@@ -1,3 +1,12 @@
 class Best50Films2018::Movie
+  @@all = []
+  attr_accessor :title
+  def initialize(title)
+    @title = title
+    @@all << self
 
+end
+def self.all
+  @@all
+end
 end
--- Best50Films2018/lib/Best50Films2018/scraper.rb
+++ Best50Films2018/lib/Best50Films2018/scraper.rb
@@ -4,11 +4,10 @@ class Best50Films2018::Scraper
     html = open(SITE)
   doc = Nokogiri::HTML(html)
   # puts doc.css("section.body-text.u-color--dark-gray.u-color--entertainment-links.font--body.has-mobile-padding.is-standard")[1..-2].text
-array_of_movies = doc.css("h2.body-text__paragraph-header.font--h2").text
- array_of_movies.each do |movie_title|
-binding.pry
- end
+title = doc.css("h2.body-text__paragraph-header.font--h2").text
+ title.each do |movie_title|
+  Best50Films2018::Movie.new(title)
 end
-
+end
 
 end
