ec47e482e2ecfbbed8e19f70463fbc4b49c7e14a
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-29 04:07:25 +0000

    message

--- Best50Films2018/lib/Best50Films2018/cli.rb
+++ Best50Films2018/lib/Best50Films2018/cli.rb
@@ -2,20 +2,22 @@ class Best50Films2018::CLI
 def run
   puts "Hello welcome to this movie info application"
   puts "Welcome to the top 55 movies"
+    puts "Please select a movie you want more info about by selecting a number below"
 Best50Films2018::Scraper.scrape_movieinfo
 list_movietitle
 end
 
 def list_movietitle
-Best50Films2018::Movie.all.each do |movie|
+@listed_movies = Best50Films2018::Movie.all.each do |movie|
   puts "#{movie.title}"
 
 end
 end
 def get_movie_method
-  puts "Please select a movie you want more info about by selecting a number below"
+index = input.to_i -1
 input = gets.strip
 if input.to_i.between?(1,55)
+  @listed_movies[index]
 else
   get_movie_method
 end
