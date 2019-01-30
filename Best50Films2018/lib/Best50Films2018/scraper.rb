3f42c498e45c3cdc94182951a069464008932645
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-30 01:12:52 +0000

    message

--- Best50Films2018/lib/Best50Films2018/scraper.rb
+++ Best50Films2018/lib/Best50Films2018/scraper.rb
@@ -4,12 +4,14 @@ class Best50Films2018::Scraper
     html = open(SITE)
   doc = Nokogiri::HTML(html)
   # puts doc.css("section.body-text.u-color--dark-gray.u-color--entertainment-links.font--body.has-mobile-padding.is-standard")[1..-2].text
-  info = doc.css("section.body-text.u-color--dark-gray.u-color--entertainment-links.font--body.has-mobile-padding.is-standard")[1..-2]
+  info = doc.css("section.body-text.u-color--dark-gray.u-color--entertainment-links.font--body.has-mobile-padding.is-standard div.body-text__content")[1..-2]
   info.each do |movie_info|
+
 attributes ={}
   attributes[:title] = movie_info.css("h2.body-text__paragraph-header.font--h2").text
 movie_info.css("p.body-text__paragraph-text.font--body.has-spacing").each do |movie_details|
-#attributes[:released] = movie_details.css()
+
+#attributes[:released] = movie_details.css("p.body-text__paragraph-text.font--body.has-spacing")[2..-2]
 
 
 #title = doc.css("h2.body-text__paragraph-header.font--h2")
