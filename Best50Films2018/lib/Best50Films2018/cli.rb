2337f8ccef64019cc5ee62d51d8de0dab40504c9
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-31 01:56:34 +0000

    cli user function working well

--- Best50Films2018/lib/Best50Films2018/cli.rb
+++ Best50Films2018/lib/Best50Films2018/cli.rb
@@ -2,9 +2,11 @@ class Best50Films2018::CLI
 def run
   puts "Hello welcome to this movie info application"
   puts "Welcome to the top 55 movies"
-    puts "Please select a movie you want more info about by selecting a number below"
+
 Best50Films2018::Scraper.scrape_movieinfo
 list_movietitle
+puts "Please select a movie you want more info about by selecting a number below"
+obtain_movie_method
 end
 
 def list_movietitle
@@ -13,14 +15,15 @@ def list_movietitle
 
 end
 end
-def get_movie_method
-index = input.to_i -1
+def obtain_movie_method
 input = gets.strip
-if input.to_i.between?(1,55)
+index = input.to_i - 1
+if index.between?(1,54)
+binding.pry
   @listed_movies[index]
 else
-  puts "Sorry I do not understand that command. Please try again."
-  get_movie_method
+  puts "Sorry that command is invalid. Please try again."
+  obtain_movie_method
 end
 end
 end
--- Best50Films2018/lib/Best50Films2018/scraper.rb
+++ Best50Films2018/lib/Best50Films2018/scraper.rb
@@ -5,11 +5,11 @@ class Best50Films2018::Scraper
   doc = Nokogiri::HTML(html)
   # puts doc.css("section.body-text.u-color--dark-gray.u-color--entertainment-links.font--body.has-mobile-padding.is-standard")[1..-2].text
   info = doc.css("section.body-text.u-color--dark-gray.u-color--entertainment-links.font--body.has-mobile-padding.is-standard div.body-text__content")[1..-2]
-  info.each do |movie_info|
+  info.each do |movie_info|                                                                  #provides title and information of each movie
 
 attributes ={}
-  attributes[:title] = movie_info.css("h2.body-text__paragraph-header.font--h2").text
-movie_info.css("p.body-text__paragraph-text.font--body.has-spacing").each do |movie_details|
+  attributes[:title] = movie_info.css("h2.body-text__paragraph-header.font--h2").text        # provides title of each movie
+movie_info.css("p.body-text__paragraph-text.font--body.has-spacing").each do |movie_details| # provides details of each movie
 
 #attributes[:released] = movie_details.css("p.body-text__paragraph-text.font--body.has-spacing")[2..-2]
 
